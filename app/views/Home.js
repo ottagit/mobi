a6b27264a8151a47cc1f86894da977069bd4d63d
ottagit <ottacc66@gmail.com>
2018-11-11 23:16:02 +0300

    apply basic styling to header

--- app/views/Home.js
+++ app/views/Home.js
@@ -6,10 +6,10 @@ export class Home extends React.Component {
   render() {
     return (
       <View>
+        <Header message = 'Press to Login' />
         <Text> This will be the home page</Text>
         <Text> This will be another section</Text>
         <Text> This will be a section too</Text>
-        <Header message = 'Press to Login' />
       </View>
     );
   }
