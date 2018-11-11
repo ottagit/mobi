94517d2f6e24e1be2d55d67d1c59cb2f43fc647f
ottagit <ottacc66@gmail.com>
2018-11-11 08:56:14 +0300

    create dummy home view

---
+++ app/views/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export class Home extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text> This will be the home page</Text>
+        <Text> This will be another section</Text>
+        <Text> This will be a section too</Text>
+      </View>
+    );
+  }
+}
