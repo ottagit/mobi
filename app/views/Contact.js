003d85891b72bf4c28763a1ce8b497a8d5b760db
ottagit <ottacc66@gmail.com>
2018-11-12 12:34:17 +0300

    add basic user input form

--- app/views/Contact.js
+++ app/views/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, TextInput, Alert } from 'react-native';
 import { Header } from '../sections/Header.js';
+import { StackNavigator } from 'react-navigation';
 
 export class Contact extends React.Component {
 
@@ -8,12 +9,60 @@ export class Contact extends React.Component {
     header: null
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: 'Enter Message',
+      name: 'Enter Name',
+      email: 'Enter your Email Address'
+    }
+  }
+
+  clearFields = () => this.setState({msg: '', name: '', email: ''});
+
+  sendMessage = () => {
+    Alert.alert(this.state.name, this.state.msg);
+    this.props.navigation.goBack();
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Header message='Login' />
-        <Text style={{flex: 8}}>The contact form will go here</Text>
-        <Text style={{flex: 6}}>More contact form goes here</Text>
+        <Text style={styles.heading}>Contact Us</Text>
+
+        <TextInput
+          style={styles.inputs}
+          onChangeText={(text) => this.setState({name: text})}
+          value={this.state.name}
+        />
+
+        <TextInput
+          style={styles.multiInput}
+          onChangeText={(text) => this.setState({msg: text})}
+          value={this.state.msg}
+          multiLine = {true}
+          numberOfLines = {4}
+        />
+
+        <TextInput
+          style={styles.inputs}
+          onChangeText={(text) => this.setState({email: text})}
+          value={this.state.email}
+        />
+
+        <TouchableHighlight onPress={this.sendMessage} underlayColor='#31e981'>
+          <Text style={styles.buttons}>
+            Send Message
+          </Text>
+        </TouchableHighlight>
+        
+        <TouchableHighlight onPress={this.clearFields} underlayColor='#31e981'>
+          <Text style={styles.buttons}>
+            Reset Form
+          </Text>
+        </TouchableHighlight>
+
       </View>
     );
   }
@@ -21,6 +70,26 @@ export class Contact extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingBottom: '45%'
+  },
+  heading: {
+    fontSize: 16,
     flex: 1
+  },
+  inputs: {
+    flex: 1,
+    width: '80%',
+    padding: 10
+  },
+  multiInput: {
+    flex: 2,
+    width: '90%',
+    paddingTop: 20
+  },
+  buttons: {
+    marginTop: 15,
+    fontSize: 16
   }
 });
